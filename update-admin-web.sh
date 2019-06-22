b40470d5e72fae94df13c157c598296ddb139793
ou8zz <ou8zz@coding.net>
2019-06-22 11:34:30 +0800

    add

--- update-admin-web.sh
+++ update-admin-web.sh
@@ -1,3 +1,3 @@
 kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-admin-web -o wide -n default
-kubectl --kubeconfig $KUBECONFIG_PROD -n default set image deployment/cailianpress-admin-web cailianpress-admin-web=registry.cn-beijing.aliyuncs.com/lanjing/cailianpress-admin-web:$1
+kubectl --kubeconfig $KUBECONFIG_PROD -n default set image deployment/cailianpress-admin-web cailianpress-admin-web=swr.cn-east-2.myhuaweicloud.com/yb7/cailianpress-admin-web:$1
 kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-admin-web -o wide -n default
--- update-web.sh
+++ update-web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-web2 -o wide -n default
-kubectl --kubeconfig $KUBECONFIG_PROD -n default set image deployment/cailianpress-web2 cailianpress-web2=registry.cn-beijing.aliyuncs.com/lanjing/cailianpress-web:$1
+kubectl --kubeconfig $KUBECONFIG_PROD -n default set image deployment/cailianpress-web2 cailianpress-web2=swr.cn-east-2.myhuaweicloud.com/yb7/cailianpress-web:$1
 kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-web2 -o wide -n default
