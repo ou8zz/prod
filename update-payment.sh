258872026fb37c4e3a696c188346d7b32b932590
ole <ole@oledeMacBook-Pro.local>
2019-07-12 20:22:00 +0800

    payment

--- update-payment.sh
+++ update-payment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-payment -o wide -n default
-kubectl --kubeconfig $KUBECONFIG_PROD -n default set image deployment/cailianpress-payment cailianpress-payment=swr.cn-east-2.myhuaweicloud.com/yb7/cailianpress-payment:$1
-kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-payment -o wide -n default
+kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-payment-v2 -o wide -n default
+kubectl --kubeconfig $KUBECONFIG_PROD -n default set image deployment/cailianpress-payment-v2 cailianpress-payment-v2=swr.cn-east-2.myhuaweicloud.com/yb7/cailianpress-payment-v2:$1
+kubectl --kubeconfig $KUBECONFIG_PROD get deployment/cailianpress-payment-v2 -o wide -n default
